5fc079b559ca6aea0f6891336187c040c270d7fd
Freya Pearse <freyapearse@gmail.com>
2023-12-27 09:48:28 -0800

    Add SVG image to home

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,10 @@
 import "./Hero.scss";
+import hero from "../../assets/images/svgs/hero.svg";
 
 export default function Hero() {
   return (
     <div className="hero">
+      <img src={hero} className="hero__image" />
       <div className="hero__titles">
         <h1 className="hero__title">Why the sadness, Squamish?</h1>
         <h2 className="hero__subtitle">Let's find some fun!</h2>
