c3490b9ba39c0e1af89578e47cac6859fd10a1af
Freya Pearse <freyapearse@gmail.com>
2023-12-26 21:39:41 -0800

    add search section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,17 @@
 import "./HomePage.scss";
 
 export default function HomePage() {
-  return <p>i am the home page</p>;
+  return (
+    <div className="homepage">
+      <div className="events">
+        <section className="events__filters">
+          <h2 className="events__search">Find your next event</h2>
+          <form>
+            <input type="text" placeholder="Search for an event" />
+            <button className="events__search-button">Search</button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 }
